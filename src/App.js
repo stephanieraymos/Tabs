2bdce4b65cdc313c960465c300952c8ce654b6d3
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-02-22 11:33:47 -0800

    Button with onClick to set the value

--- src/App.js
+++ src/App.js
@@ -25,28 +25,37 @@ function App() {
       </section>
     );
   }
-  const {company, dates, duties, title} = jobs[value];
+  const { company, dates, duties, title } = jobs[value];
 
-  return <section className="section">
+  return (
+    <section className="section">
       <div className="title">
-          <h2>Experience</h2>
-          <div className="underline"></div>
+        <h2>Experience</h2>
+        <div className="underline"></div>
       </div>
       <div className="jobs-center">
-          {/* btn container */}
-          <article className="job-info">
-              <h3>{title}</h3>
-              <h4>{company}</h4>
-              <p className="job-date">{dates}</p>
-              {duties.map((duty, index) => {
-                  return <div key={index} className="job-desc">
-                      <FaAngleDoubleRight className="job-icon" />
-                      <p>{duty}</p>
-                  </div>
-              })}
-          </article>
+        {/* btn container */}
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return <button key={item.id} onClick={() => setValue(index)}>{item.company}</button>;
+          })}
+        </div>
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon" />
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
       </div>
-  </section>;
+    </section>
+  );
 }
 
 export default App;
