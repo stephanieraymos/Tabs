f2a341a76a39d37d05d7dcc0bd70e66c7ecbdeb4
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-02-22 11:26:25 -0800

    Data display

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [value, setValue] = useState(0);
 
   const fetchJobs = async () => {
-    const reponse = await fetch(url);
+    const response = await fetch(url);
     const newJobs = await response.json();
     setJobs(newJobs);
     setLoading(false);
@@ -18,13 +18,35 @@ function App() {
     fetchJobs();
   }, []);
 
-  if(loading){
-      return <section className="section loading">
-          <h1>Loading ...</h1>
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>Loading ...</h1>
       </section>
+    );
   }
+  const {company, dates, duties, title} = jobs[value];
 
-  return <h2>Tabs</h2>;
+  return <section className="section">
+      <div className="title">
+          <h2>Experience</h2>
+          <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+          {/* btn container */}
+          <article className="job-info">
+              <h3>{title}</h3>
+              <h4>{company}</h4>
+              <p className="job-date">{dates}</p>
+              {duties.map((duty, index) => {
+                  return <div key={index} className="job-desc">
+                      <FaAngleDoubleRight className="job-icon" />
+                      <p>{duty}</p>
+                  </div>
+              })}
+          </article>
+      </div>
+  </section>;
 }
 
 export default App;
